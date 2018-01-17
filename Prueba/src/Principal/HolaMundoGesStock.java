e416bf23a5be6deaaa6c899c71b2635e912ea629
albertopareja <35523536+albertopareja@users.noreply.github.com>
2018-01-17 10:49:44 +0100

    Update HolaMundoGesStock.java

--- Prueba/src/Principal/HolaMundoGesStock.java
+++ Prueba/src/Principal/HolaMundoGesStock.java
@@ -3,7 +3,7 @@ package Principal;
 public class HolaMundoGesStock {
 
 	public static void main(String[] args) {
-		System.out.println("Tira bien, parece");
+		System.out.println("Tira bien, parece o eso creo. Ahora pongo Version 1.1");
 
 	}
 
